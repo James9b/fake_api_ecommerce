cd890e2fe644eaa9f828ad23e5e7f0e96790b83e
Jitesh Bhatia <74812666+James9b@users.noreply.github.com>
2025-12-29 23:17:56 +0530

    Delete src directory

--- src/components/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-function LoadingSpinner({ size = 'medium' }) {
-    return (
-        <div className={`loading-spinner loading-spinner-${size}`}>
-            <div className="spinner"></div>
-        </div>
-    )
-}
-
-export default LoadingSpinner
